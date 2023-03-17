774b44da4a3bbd2a4790601fa9f81fd230f0a777
Pouish85 <pouish.iphone@gmail.com>
2023-03-17 11:48:19 +0100

    feat: searchbar with searching tool

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import styles from './Recipe.module.scss';
-import recipeImg from '../assets/images/pavlova.jpg';
 
-export default function Recipe() {
-    return <div className={styles.recipe}>
+export default function Recipe({title, img}) {
+    
+    console.log("Rendu du composant")
+    const [liked, setLiked] = useState(false);
+
+    function handleLikeClick() {
+        setLiked(!liked)
+    }
+
+    
+    return (
+        <div onClick={() => setLiked(handleLikeClick)} className={styles.recipe}>
             <div className={styles.recipeImg}>
-                <img src={recipeImg} alt="Image Recette" />
+                <img src={img} alt="Image Recette" />
             </div>
-            <div className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}>
-                <h3>Pavlova Express</h3>
+            <div className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}>
+                <h3 className='mb-20'>{title}</h3>
+                <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
             </div>
         </div>
+        )
 }
--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,19 +1,26 @@
 import Recipe from '../Recipe';
 import styles from './Content.module.scss';
+import {data} from '../../data/recipes';
+import { useState } from 'react';
 
 export default function Content() {
+
+    const recipes = data;
+    const [filter, setFilter] = useState('');
+
+    function handleInput (event) {
+        setFilter(event.target.value.trim().toLowerCase());
+    }
+
     return <div className="flex-fill container">
-        <h1 className="p-20">Decouvrez nos nouvelles recettes</h1>
-            <div className={`card p-20 mb-20 ${styles.contentCard}`}>
+        <h1 className="my-30">Decouvrez nos nouvelles recettes</h1>
+            <div className={`card p-20 my-30 d-flex flex-column ${styles.contentCard}`}>
+                <div className={`d-flex flex-row justify-content-center align-items-center my-30 ${styles.searchbar}`}>
+                    <i className="fa-solid fa-magnifgying-glass"></i>
+                    <input onInput={handleInput} type="text" name='search' id='search' className='flex-fill' placeholder='Search' />
+                </div>
                 <div className={styles.grid}>
-                    <Recipe />
-                    <Recipe />
-                    <Recipe />
-                    <Recipe />
-                    <Recipe />
-                    <Recipe />
-                    <Recipe />
-                    <Recipe />
+                    {recipes.filter(recipe => recipe.title.toLowerCase().startsWith(filter)).map(({_id, title, image}) => <Recipe key={_id} title={title} img={image} />)}
                 </div>
         </div>
     </div>
